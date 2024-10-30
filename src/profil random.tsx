175935ac50ad626605696f592eb0a5e22dd9ace7
ALEXANDRE.CLASQUIN <alexandre.clasquin@gmail.com>
2024-10-30 15:33:24 +0100

    fisrt depot

---
+++ src/profil random.tsx
@@ -0,0 +1,15 @@
+
+const employee = {
+    "results": [
+        {
+            "name": {
+                "first": "Heidi",
+                "last": "Hausner"
+            },
+            "email": "heidi.hausner@example.com",
+            "picture": {
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/women/66.jpg"
+            }
+        }
+    ]
+}
